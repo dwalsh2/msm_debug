5211220f13ba3ae33a305ba5da4000762d04e045
Dan Walsh <dwalsh2@chicagobooth.edu>
2017-07-26 05:00:31 +0000

    solved debug

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,47 +1,57 @@
 class DirectorsController < ApplicationController
   def index
-    directors = Director.all
+    @directors = Director.all
+    
+    render("directors/index.html.erb")
   end
 
   def show
-    @director = Director.find(params[:id])
+    @director = Director.find(params["id"])
+    
+    render("directors/show_details.html.erb")
   end
 
-  def new
+  def new_form
+    
+    render("directors/new_form.html.erb")
   end
 
   def create_row
     @director = Director.new
-    @director.dob = params[:dob]
-    @director.name = params[:name]
-    @director.bio = params[:bio]
-    @director.image_url = params[:image_url]
+    @director.dob = params["dob"]
+    @director.name = params["name"]
+    @director.bio = params["bio"]
+    @director.image_url = params["image_url"]
 
     @director.save
 
-    render("show")
+    redirect_to("/directors")
   end
 
   def edit_form
-    @director = Director.find(params[:id])
+    @director = Director.find(params["id"])
+    
+    render("directors/edit_form.html.erb")
   end
 
   def update_row
-    @director = Director.find(params[:id])
+    @director = Director.find(params["id"])
 
-    @director.dob = params[":dob"]
-    @director.name = params[":name"]
-    @director.bio = params[":bio"]
-    @director.image_url = params[":image_url"]
+    @director.dob = params["dob"]
+    @director.name = params["name"]
+    @director.bio = params["bio"]
+    @director.image_url = params["image_url"]
 
     @director.save
 
-    render("show")
+    redirect_to("/directors")
   end
 
   def destroy
-    @director = Director.find(params[:id])
+    @director = Director.find(params["id"])
 
     @director.destroy
+    
+    render("directors/destroy_row.html.erb")
   end
 end
